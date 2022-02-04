89350d20a3a7ba8933c98003b7af7462c88a47d6
cipher-vasu <guptavasu260500@gmail.com>
2022-02-04 20:27:58 +0530

    Learned how to Delete Middle Element in a Stack

---
+++ Recursion/Delete Middle Element of a Stack.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void remove_middle(stack<int>& st, int k)
+{
+    if (k == 1)
+    {
+        st.pop();
+        return;
+    }
+    int temp = st.top();
+    st.pop();
+    remove_middle(st, k - 1);
+    st.push(temp);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        st.push(a);
+    }
+    int k = st.size() / 2 + 1;
+    remove_middle(st, k);
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    return 0;
+}
+
