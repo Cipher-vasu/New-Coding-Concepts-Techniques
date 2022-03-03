3d603218ae4b828546b46fb705a22a5216f46946
cipher-vasu <guptavasu260500@gmail.com>
2022-03-03 21:49:14 +0530

    pushed code of pemutation of string with case change

---
+++ Recursion/Permutation of a string with case change.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void helper(string a, string temp, vector<string>& ans)
+{
+	if (a.size() == 0)
+	{
+		ans.push_back(temp);
+		return;
+	}
+	string temp1 = temp;
+	string temp2 = temp;
+	temp2.push_back(a[0] - 32);
+	temp1.push_back(a[0]);
+	a.erase(a.begin() + 0);
+	helper(a, temp1, ans);
+	helper(a, temp2, ans);
+
+}
+
+int main()
+{
+	string a;
+	cin >> a;
+	string temp = "";
+	vector<string> ans;
+	helper(a, temp, ans);
+	for (int i = 0; i < ans.size(); i++)
+	{
+		cout << ans[i] << endl;
+	}
+}
